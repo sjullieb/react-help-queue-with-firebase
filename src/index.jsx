cc694f4c9c72f99e27754e493accb04442bdad1f
Yulia Shidlovskaya <yulia@>
2019-05-15 10:46:56 -0700

    refactor: remove local storage firebase

--- src/index.jsx
+++ src/index.jsx
@@ -5,20 +5,21 @@ import { HashRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
-import persistDataLocally from './middleware/persist-data-locally';
+//import persistDataLocally from './middleware/persist-data-locally';
 
-let retrievedState;
-try{
-  retrievedState = localStorage.getItem('reduxStore');
-  if(retrievedState === null){
-    retrievedState = {};
-  }
-  retrievedState = JSON.parse(retrievedState);
-} catch(err){
-  retrievedState = {};
-}
+// let retrievedState;
+// try{
+//   retrievedState = localStorage.getItem('reduxStore');
+//   if(retrievedState === null){
+//     retrievedState = {};
+//   }
+//   retrievedState = JSON.parse(retrievedState);
+// } catch(err){
+//   retrievedState = {};
+// }
 
-const store = createStore(rootReducer, retrievedState, applyMiddleware(persistDataLocally));
+// const store = createStore(rootReducer, retrievedState, applyMiddleware(persistDataLocally));
+const store = createStore(rootReducer);
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
