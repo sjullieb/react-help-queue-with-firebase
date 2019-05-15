5a13951cc803ea1915b8ba050a2ff49ea243b0b1
Yulia Shidlovskaya <yulia@>
2019-05-15 10:03:42 -0700

    add data to local storage

---
+++ src/middleware/persist-data-locally.js
@@ -0,0 +1,8 @@
+const persistDataLocally = store => next => action => {
+
+    next(action);
+    localStorage['reduxStore'] = JSON.stringify(store.getState());
+    console.log('Local Storage:', localStorage['reduxStore']);
+}
+
+export default persistDataLocally;
